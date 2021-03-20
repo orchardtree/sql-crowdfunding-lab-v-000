65825a8eba367dbde9efcf7c1730a04e21738a1e
Jesse Wenger <wengerj@gmail.com>
2021-03-20 17:03:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,12 +34,12 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
-    FROM projects
-    INNER JOIN pledges
-    ON projects.id = pledges.project_id
-    GROUP BY projects.id
-    HAVING SUM(pledges.amount) >= projects.funding_goal;"
+  "SELECT users.name, SUM(pledges.amount)
+    FROM users
+    JOIN pledges
+    ON users.id = pledges.user_id
+    GROUP BY users.id
+    ORDER BY pledges.amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
